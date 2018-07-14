78df22a64622d0554a6004e4b08f29a5355899fa
Daniel-Vindio <32176509+Daniel-Vindio@users.noreply.github.com>
2018-07-14 18:57:11 +0200

    Add files via upload

---
+++ 3-inst-basic-sys-sw/help-inst-gzip.sh
@@ -0,0 +1,54 @@
+# Help to install gzip with qi
+
+#6.64. gzip
+
+
+T_COMIENZO=$(date +"%T")	#para calcular el tiempo de instalación
+
+function registro_error () {
+#Función para registrar errores y resultados en la bitaćora
+if [ $? -ne 0 ]
+then
+	MOMENTO=$(date)
+	echo $MSG_ERR
+	echo "$MOMENTO $nombre <$1> -> ERROR" >> $FILE_BITACORA
+	exit 1
+else
+	MOMENTO=$(date)
+	echo "$MOMENTO $nombre <$1> -> Conforme" >> $FILE_BITACORA
+fi
+}
+
+control_flujo () {
+	echo "Continue? To exit press N"
+	read CS
+	if [ "$CS" == "N" ]
+		then
+			echo "exit"
+			exit
+	fi
+}
+
+if [ $(id -u) -ne 0 ]
+	then 
+		echo -e "Ur not root bro"
+		echo -e "Tines que ser root, tío"
+	exit 1
+fi
+
+#---------------------------------------
+qi -i gzip-$VER_gzip-i686+1.tlz
+mv -v /usr/bin/gzip /bin
+
+
+
+
+#Registro de tiempos de ejecución
+T_FINAL=$(date +"%T")
+echo "$(date) $nombre <$MSG_TIME> $T_COMIENZO $T_FINAL" >> $FILE_BITACORA
+
+
+
+
+
+
